ba16a70d49ea16b0f51d45f450eb31f3ae3a08cb
BugHunter-X <askzenai@gmail.com>
2025-01-17 03:38:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+// Corrected use of Tailwind directives
+<div class="bg-blue-500 p-4 text-white">
+  <button class="hover:bg-gray-500 focus:outline-none">
+    Click me
+  </button>
+</div>
+```
